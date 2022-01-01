15dd550389042c47f67ad907e6cf6aa1669590db
johnson <breaking.lover@gmail.com>
2021-12-31 21:30:38 -0500

    added api call for market info

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
       </div>
       <div className="main">
         <Layout>
-          <div className="main">
+          <div className="routes">
             <Switch>
               <Route exact path="/">
                 <Homepage />
@@ -40,8 +40,21 @@ const App = () => {
             </Switch>
           </div>
         </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Cryptoverse <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
       </div>
-      <div className="footer"></div>
     </div>
   );
 };
